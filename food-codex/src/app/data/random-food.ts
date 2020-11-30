3f26eba62ee7ad3a791ae7bbf0c9f3e5f9e86f45
Matthew Corpus <mbcorpus@uci.edu>
2020-11-29 20:19:02 -0800

    Fixed looping in random-food.ts
    Looping over the ingredients and measurements now works correctly, empty ingredients/measurements will not show

--- food-codex/src/app/data/random-food.ts
+++ food-codex/src/app/data/random-food.ts
@@ -10,140 +10,21 @@ export class RandomFood {
     nationality:string;
 
     constructor(object:any){
+      console.log(object);
         this.name = object.strMeal;
         this.id = object.idMeal;
         this.category = object.strCategory;
         this.ingredient = [];
         this.measure = [];
-        // for(var i = 0; i < 20; i++){ // Not sure how to loop this...
-        //     this.ingredient.push(object.strIngredient1);
-        // }
 
-        // StrIngredient
-        if(object.strIngredient1 !== "" && object.strIngredient1 !== null){
-            this.ingredient.push(object.strIngredient1);
-        }
-        if(object.strIngredient2 !== "" && object.strIngredient2 !== null){
-            this.ingredient.push(object.strIngredient2);
-        }
-        if(object.strIngredient3 !== "" && object.strIngredient3 !== null){
-            this.ingredient.push(object.strIngredient3);
-        }
-        if(object.strIngredient4 !== "" && object.strIngredient4 !== null){
-            this.ingredient.push(object.strIngredient4);
-        }
-        if(object.strIngredient5 !== "" && object.strIngredient5 !== null){
-            this.ingredient.push(object.strIngredient5);
-        }
-        if(object.strIngredient6 !== "" && object.strIngredient6 !== null){
-            this.ingredient.push(object.strIngredient6);
-        }
-        if(object.strIngredient7 !== "" && object.strIngredient7 !== null){
-            this.ingredient.push(object.strIngredient7);
-        }
-        if(object.strIngredient8 !== "" && object.strIngredient8 !== null){
-            this.ingredient.push(object.strIngredient8);
-        }
-        if(object.strIngredient9 !== "" && object.strIngredient9 !== null){
-            this.ingredient.push(object.strIngredient9);
-        }
-        if(object.strIngredient10 !== "" && object.strIngredient10 !== null){
-            this.ingredient.push(object.strIngredient10);
-        }
-        if(object.strIngredient11 !== "" && object.strIngredient11 !== null){
-            this.ingredient.push(object.strIngredient11);
-        }
-        if(object.strIngredient12 !== "" && object.strIngredient12 !== null){
-            this.ingredient.push(object.strIngredient12);
-        }
-        if(object.strIngredient13 !== "" && object.strIngredient13 !== null){
-            this.ingredient.push(object.strIngredient13);
-        }
-        if(object.strIngredient14 !== "" && object.strIngredient14 !== null){
-            this.ingredient.push(object.strIngredient14);
-        }
-        if(object.strIngredient15 !== "" && object.strIngredient15 !== null){
-            this.ingredient.push(object.strIngredient15);
-        }
-        if(object.strIngredient16 !== "" && object.strIngredient16 !== null){
-            this.ingredient.push(object.strIngredient16);
-        }
-        if(object.strIngredient17 !== "" && object.strIngredient17 !== null){
-            this.ingredient.push(object.strIngredient17);
-        }
-        if(object.strIngredient18 !== "" && object.strIngredient18 !== null){
-            this.ingredient.push(object.strIngredient18);
-        }
-        if(object.strIngredient19 !== "" && object.strIngredient19 !== null){
-            this.ingredient.push(object.strIngredient19);
-        }
-        if(object.strIngredient20 !== "" && object.strIngredient20 !== null){
-            this.ingredient.push(object.strIngredient20);
-        }
-
-        //StrMeasure
-        if(object.strMeasure1 !== "" && object.strMeasure1 !== null){
-            this.measure.push(object.strMeasure1);
-        }
-        if(object.strMeasure2 !== "" && object.strMeasure2 !== null){
-            this.measure.push(object.strMeasure2);
-        }
-        if(object.strMeasure3 !== "" && object.strMeasure3 !== null){
-            this.measure.push(object.strMeasure3);
-        }
-        if(object.strMeasure4 !== "" && object.strMeasure4 !== null){
-            this.measure.push(object.strMeasure4);
-        }
-        if(object.strMeasure5 !== "" && object.strMeasure5 !== null){
-            this.measure.push(object.strMeasure5);
-        }
-        if(object.strMeasure6 !== "" && object.strMeasure6 !== null){
-            this.measure.push(object.strMeasure6);
-        }
-        if(object.strMeasure7 !== "" && object.strMeasure7 !== null){
-            this.measure.push(object.strMeasure7);
-        }
-        if(object.strMeasure8 !== "" && object.strMeasure8 !== null){
-            this.measure.push(object.strMeasure8);
-        }
-        if(object.strMeasure9 !== "" && object.strMeasure9 !== null){
-            this.measure.push(object.strMeasure9);
-        }
-        if(object.strMeasure10 !== "" && object.strMeasure10 !== null){
-            this.measure.push(object.strMeasure10);
-        }
-        if(object.strMeasure11 !== "" && object.strMeasure11 !== null){
-            this.measure.push(object.strMeasure11);
-        }
-        if(object.strMeasure12 !== "" && object.strMeasure12 !== null){
-            this.measure.push(object.strMeasure12);
-        }
-        if(object.strMeasure13 !== "" && object.strMeasure13 !== null){
-            this.measure.push(object.strMeasure13);
-        }
-        if(object.strMeasure14 !== "" && object.strMeasure14 !== null){
-            this.measure.push(object.strMeasure14);
-        }
-        if(object.strMeasure15 !== "" && object.strMeasure15 !== null){
-            this.measure.push(object.strMeasure15);
-        }
-        if(object.strMeasure16 !== "" && object.strMeasure16 !== null){
-            this.measure.push(object.strMeasure16);
-        }
-        if(object.strMeasure17 !== "" && object.strMeasure17 !== null){
-            this.measure.push(object.strMeasure17);
-        }
-        if(object.strMeasure18 !== "" && object.strMeasure18 !== null){
-            this.measure.push(object.strMeasure18);
-        }
-        if(object.strMeasure19 !== "" && object.strMeasure19 !== null){
-            this.measure.push(object.strMeasure19);
-        }
-        if(object.strMeasure20 !== "" && object.strMeasure20 !== null){
-            this.measure.push(object.strMeasure20);
-        }
-
-        // There are measurements for each ingredients. It's up to you guys if you want to add them. <-- ya
+        Object.keys(object).forEach((element) => {
+          if (element.includes("strIngredient") && !(object[element] === "") && !(object[element] === null)) {
+            this.ingredient.push(object[element]);
+          }
+          if (element.includes("strMeasure") && !(object[element] === "") && !(object[element] === null)) {
+            this.measure.push(object[element]);
+          }
+        })
 
         this.instruction = object.strInstructions;
         this.imageURL = object.strMealThumb;
